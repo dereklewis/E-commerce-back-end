b112f8c63559c6d096e9a5e0938265d669b6fd3f
Derek Lewis <derek.lewis.den@outlook.com>
2021-10-03 12:01:31 -0600

    push for assistance

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,28 +23,20 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// create new product
 router.post("/", async (req, res) => {
   try {
     const productData = await Product.create({
       id: "",
-      product_name: "Basketball",
+      product_name: "v-neck",
       price: 200.0,
       stock: 3,
+      category_id: 1,
     });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
 
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -109,8 +101,23 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No location found with this id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
