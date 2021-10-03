8cf136f8c4c68cc4e28049c4e9ca32c14e84b000
Derek Lewis <derek.lewis.den@outlook.com>
2021-10-02 18:21:01 -0600

    post, put and delete routes for tag-routes completed and tested

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,7 +25,9 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const categoryData = await Category.create(req.body);
+    const categoryData = await Category.create({
+      category_name: "knickers",
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
